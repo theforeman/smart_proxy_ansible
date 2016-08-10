aaddcf4a64c48e177f458af5bb78cddaee72ec02
Adam Ruzicka <aruzicka@redhat.com>
2016-08-10 09:16:00 +0200

    Fixes #16468 - integrate foreman_ansible_core
    This allows running playbooks via proxy

--- lib/smart_proxy_ansible/plugin.rb
+++ lib/smart_proxy_ansible/plugin.rb
@@ -4,7 +4,17 @@ module Proxy::Ansible
     https_rackup_path File.expand_path("http_config.ru", File.expand_path("../", __FILE__))
 
     settings_file "ansible.yml"
-    default_settings :ansible_working_dir => '~/.foreman-ansible'
     plugin :ansible, Proxy::Ansible::VERSION
+
+    after_activation do
+      begin
+        require 'smart_proxy_dynflow_core'
+        require 'foreman_ansible_core'
+        ForemanAnsibleCore.initialize_settings(Proxy::Ansible::Plugin.settings.to_h)
+      rescue LoadError => _
+        # Dynflow core is not available in the proxy, will be handled
+        # by standalone Dynflow core
+      end
+    end
   end
 end
