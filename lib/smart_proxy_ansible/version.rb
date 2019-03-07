9f34f2f04a6670c52b4b951d2cef3746d7a43275
Marek Hulan <mhulan@redhat.com>
2019-03-07 16:54:52 +0100

    Release 2.1.2

--- lib/smart_proxy_ansible/version.rb
+++ lib/smart_proxy_ansible/version.rb
@@ -2,6 +2,6 @@ module Proxy
   # Version, this allows the proxy and other plugins know
   # what version of the Ansible plugin is running
   module Ansible
-    VERSION = '2.1.1'
+    VERSION = '2.1.2'
   end
 end
