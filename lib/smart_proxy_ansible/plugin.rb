5e5306ed16b1a494bc13d70375c1ad72d737c18c
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-01-29 14:30:07 +0100

    Use rackup_path as introduced in Foreman Proxy 2.3
    rackup_path does the same as calling both http_rackup_path and
    https_rackup_path. It also uses the __dir__ shorthand.

--- lib/smart_proxy_ansible/plugin.rb
+++ lib/smart_proxy_ansible/plugin.rb
@@ -2,11 +2,7 @@ module Proxy
   module Ansible
     # Calls for the smart-proxy API to register the plugin
     class Plugin < Proxy::Plugin
-      http_rackup_path File.expand_path('http_config.ru',
-                                        File.expand_path('../', __FILE__))
-      https_rackup_path File.expand_path('http_config.ru',
-                                         File.expand_path('../', __FILE__))
-
+      rackup_path File.expand_path('http_config.ru', __dir__)
       settings_file 'ansible.yml'
       plugin :ansible, Proxy::Ansible::VERSION
 
