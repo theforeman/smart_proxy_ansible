9a5d2f361ed005c0171c8e7f96e7bab544963ffc
yifatmakias <ymakias@redhat.com>
2021-07-26 12:19:45 +0300

    Fixes #33133 - Fix extraction of variables when import role from collection

--- lib/smart_proxy_ansible/api.rb
+++ lib/smart_proxy_ansible/api.rb
@@ -35,8 +35,8 @@ module Proxy
         role_name_parts = role_name.split('.')
         if role_name_parts.count == 3
           RolesReader.collections_paths.split(':').each do |path|
-            variables[role_name] ||= VariablesExtractor
-                                   .extract_variables("#{path}/ansible_collections/#{role_name_parts[0]}/#{role_name_parts[1]}/roles/#{role_name_parts[2]}")
+            variables[role_name] = VariablesExtractor
+                                   .extract_variables("#{path}/ansible_collections/#{role_name_parts[0]}/#{role_name_parts[1]}/roles/#{role_name_parts[2]}") if variables[role_name].nil? || variables[role_name].empty?
           end
         else
           RolesReader.roles_path.split(':').each do |path|
