70af2956015e298e203499ff218a6945c9fb1e0d
aabramov <aabramov@redhat.com>
2021-08-26 12:02:24 +0300

    Fixes #32814 - Fix working dir when doesn't exist

--- lib/smart_proxy_ansible.rb
+++ lib/smart_proxy_ansible.rb
@@ -4,6 +4,8 @@ module Proxy
   # Basic requires for this plugin
   module Ansible
     require 'smart_proxy_ansible/version'
+    require 'smart_proxy_ansible/configuration_loader'
+    require 'smart_proxy_ansible/validate_settings'
     require 'smart_proxy_ansible/plugin'
     require 'smart_proxy_ansible/roles_reader'
     require 'smart_proxy_ansible/variables_extractor'
