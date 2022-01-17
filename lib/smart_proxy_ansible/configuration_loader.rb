fc132db21ed5f7278c86231993752118265f2aa3
Adam Ruzicka <aruzicka@redhat.com>
2022-01-17 16:13:17 +0100

    Fixes #32709 - Drop all code related to ansible-playbook

--- lib/smart_proxy_ansible/configuration_loader.rb
+++ lib/smart_proxy_ansible/configuration_loader.rb
@@ -4,17 +4,10 @@ module Proxy::Ansible
       require 'smart_proxy_dynflow'
       require 'smart_proxy_dynflow/continuous_output'
       require 'smart_proxy_ansible/task_launcher/ansible_runner'
-      require 'smart_proxy_ansible/task_launcher/playbook'
-      require 'smart_proxy_ansible/actions'
-      require 'smart_proxy_ansible/remote_execution_core/ansible_runner'
       require 'smart_proxy_ansible/runner/ansible_runner'
-      require 'smart_proxy_ansible/runner/command_creator'
-      require 'smart_proxy_ansible/runner/playbook'
 
       Proxy::Dynflow::TaskLauncherRegistry.register('ansible-runner',
                                                     TaskLauncher::AnsibleRunner)
-      Proxy::Dynflow::TaskLauncherRegistry.register('ansible-playbook',
-                                                    TaskLauncher::Playbook)
     end
   end
 end
