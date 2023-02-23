theforeman/smart_proxy_ansible

commit fc161ff33ebe1cc955d77d389a2296c2ed70671e

Refs #35456 - Expect the wrapper to exist on filesystem

bin/ansible-runner-environment.sh
@@ -1,0 +1,4 @@
+#!/bin/sh
+
+[ -f "$ANSIBLE_ENVIRONMENT_FILE" ] && source "$ANSIBLE_ENVIRONMENT_FILE"
+exec "$@"

lib/smart_proxy_ansible/runner/ansible_runner.rb
@@ -10,6 +10,9 @@ module Proxy::Ansible
       include ::Proxy::Dynflow::Runner::ProcessManagerCommand
       attr_reader :execution_timeout_interval
 
+      # To make this overridable in development
+      ENVIRONMENT_WRAPPER = ENV['SMART_PROXY_ANSIBLE_ENVIRONMENT_WRAPPER'] || '/usr/libexec/ansible-runner-environment'
+
       def initialize(input, suspended_action:, id: nil)
         super input, :suspended_action => suspended_action, :id => id
         @inventory = rebuild_secrets(rebuild_inventory(input), input)
@@ -186,20 +189,12 @@ module Proxy::Ansible
       def start_ansible_runner
         env = {}
         env['FOREMAN_CALLBACK_DISABLE'] = '1' if @rex_command
+        env['ANSIBLE_ENVIRONMENT_FILE'] = Proxy::Ansible::Plugin.settings[:ansible_environment_file]
         command = ['ansible-runner', 'run', @root, '-p', 'playbook.yml']
         command << '--cmdline' << cmdline unless cmdline.nil?
         command << verbosity if verbose?
 
-        ansible_env = Proxy::Ansible::Plugin.settings[:ansible_environment_file].shellescape
-        init = <<~EOF
-          #!/bin/sh
-          [ -f #{ansible_env} ] && source #{ansible_env}
-          exec #{command.map(&:shellescape).join(' ')}
-        EOF
-        init_path = File.join(@root, 'init.sh')
-        File.write(init_path, init, { perm: 0o0755 })
-
-        initialize_command(env, init_path)
+        initialize_command(env, ENVIRONMENT_WRAPPER, *command)
         logger.debug("[foreman_ansible] - Running command '#{command.join(' ')}'")
       end
 

commit 71abdf9f9292f447fd92c6022b5785c39c9ea108

Fixes #36141 - use `.` to source the env file, source is a bashism
`source` is a bashism that doesn't exist in `dash` and other POSIX
shells.

Fixes: fc161ff33ebe1cc955d77d389a2296c2ed70671e

bin/ansible-runner-environment.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-[ -f "$SMART_PROXY_ANSIBLE_ENVIRONMENT_FILE" ] && source "$SMART_PROXY_ANSIBLE_ENVIRONMENT_FILE"
+[ -f "$SMART_PROXY_ANSIBLE_ENVIRONMENT_FILE" ] && . "$SMART_PROXY_ANSIBLE_ENVIRONMENT_FILE"
 exec "$@"