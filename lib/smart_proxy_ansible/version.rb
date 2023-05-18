75d399b1ce6d82b027db47a1b53b77740ce80e2a
nofaralfasi <nalfassi@redhat.com>
2023-05-18 11:23:48 +0300

    Bump version to 3.5.5

--- lib/smart_proxy_ansible/version.rb
+++ lib/smart_proxy_ansible/version.rb
@@ -2,6 +2,6 @@ module Proxy
   # Version, this allows the proxy and other plugins know
   # what version of the Ansible plugin is running
   module Ansible
-    VERSION = '3.5.4'
+    VERSION = '3.5.5'
   end
 end
