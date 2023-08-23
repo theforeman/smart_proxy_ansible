d9102e97d7819b20bde100d4700fc8547e78b1f9
nofaralfasi <nalfassi@redhat.com>
2023-08-23 12:14:26 +0300

    Fixes #36341 - Refactor AnsibleRunner and Introduce ArtifactsProcessor
    This commit enhances the AnsibleRunner to improve functionality,
    and introduces the ArtifactsProcessor for better artifact handling.

--- lib/smart_proxy_ansible/configuration_loader.rb
+++ lib/smart_proxy_ansible/configuration_loader.rb
@@ -4,6 +4,7 @@ module Proxy::Ansible
       require 'smart_proxy_dynflow'
       require 'smart_proxy_dynflow/continuous_output'
       require 'smart_proxy_ansible/task_launcher/ansible_runner'
+      require 'smart_proxy_ansible/artifacts_processor'
       require 'smart_proxy_ansible/runner/ansible_runner'
 
       Proxy::Dynflow::TaskLauncherRegistry.register('ansible-runner',
